0ad35eaab4982dcc0162f7c78d68fb7ff05fb302
cameronl10 <cameron.lee@smus.ca>
2020-10-14 20:49:01 -0700

    Initial commit

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/teamcode/mecanumDrive.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.robotcontroller.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+public class mecanumDrive extends OpMode {
+    private DcMotor backLeftDrive = null;
+    private DcMotor backRightDrive = null;
+    private DcMotor frontLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
