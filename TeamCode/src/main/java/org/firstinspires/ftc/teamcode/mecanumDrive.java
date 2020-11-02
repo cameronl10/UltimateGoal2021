2f757764d4fa63fbddd000897ddfa4f8e7239b3a
cameronl10 <cameron.lee@smus.ca>
2020-11-02 13:57:31 -0800

    Drive code updated + arm and servo code added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumDrive.java
@@ -1,20 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
-
+@TeleOp(name = "mecanum drive")
 public class mecanumDrive extends OpMode {
     private DcMotor backLeftDrive = null;
     private DcMotor backRightDrive = null;
     private DcMotor frontLeftDrive = null;
     private DcMotor frontRightDrive = null;
+    private DcMotor armMotorLeft = null;
+    private DcMotor armMotorRight = null;
+    private Servo armServo = null;
     @Override
     public void init() {
-
+        backLeftDrive =hardwareMap.get(DcMotor.class,"");
+        backRightDrive = hardwareMap.get(DcMotor.class,"");
+        frontLeftDrive = hardwareMap.get(DcMotor.class,"");
+        frontRightDrive = hardwareMap.get(DcMotor.class,"");
+        armMotorLeft = hardwareMap.get(DcMotor.class,"");
+        armMotorRight = hardwareMap.get(DcMotor.class,"");
+        armServo = hardwareMap.get(Servo.class,"");
     }
 
     @Override
     public void loop() {
+
+        /*
+        Driving Code in gamepad one
+         */
         float gamepad1LeftY = -gamepad1.left_stick_y;
         float gamepad1LeftX = gamepad1.left_stick_x;
         float gamepad1RightX = gamepad1.right_stick_x;
@@ -23,7 +38,7 @@ public class mecanumDrive extends OpMode {
         float FrontRight = gamepad1LeftY - gamepad1LeftX + gamepad1RightX;
         float BackLeft = -gamepad1LeftY + gamepad1LeftX + gamepad1RightX;
         float BackRight = gamepad1LeftY + gamepad1LeftX + gamepad1RightX;
-
+        //set ranges that the wheels can go
         FrontRight = Range.clip(FrontRight, -1, 1);
         FrontLeft = Range.clip(FrontLeft, -1, 1);
         BackLeft = Range.clip(BackLeft, -1, 1);
@@ -43,7 +58,33 @@ public class mecanumDrive extends OpMode {
         telemetry.addData("b left pwr", "back left pwr: " + String.format("%.2f", BackLeft));
 
 
-        
+        /*
+        Arm code in gamepad two
+         */
+        //conditional statements to check if input towards arm is active, pressing y raises arm, pressing a lowers arm, no inputs brakes the arm.
+        if(gamepad2.y){
+            armMotorLeft.setPower(1 * 0.3);
+            armMotorRight.setPower(1*-0.3);
+        }else  if(gamepad2.a){
+            armMotorLeft.setPower(1*-0.3);
+            armMotorRight.setPower(1*0.3);
+        }else{
+            armMotorLeft.setPower(0);
+            armMotorRight.setPower(0);
+            armMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            armMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        }
+        //set servo position to  180 degrees
+        if(gamepad2.right_bumper){
+            armServo.setPosition(1);
+        }
+        //set servo position to 0 degrees
+        if(gamepad2.left_bumper){
+            armServo.setPosition(0);
+        }
+
+        telemetry.addData("arm motor","arm motor power" + String.format("%.2f",armMotorLeft.getPower()));
+        telemetry.addData("servo psn","right servo position" + String.format("%.2f",armServo.getPosition()));
 
 
     }
